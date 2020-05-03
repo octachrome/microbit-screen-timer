1c8b3d1a5ac0029cfa49012d6abf5b1115fccda9
Chris Brown <chris@thebrown.net>
2020-05-03 16:23:11 +0100

    More alarms

--- screen-timer.py
+++ screen-timer.py
@@ -1,45 +1,103 @@
-# Add your Python code here. E.g.
-import time
+import utime
 from microbit import *
 
-def get_time_image(mins):
-    if mins <= 9:
-        return mins
-    image = ""
-    for i in range(29):
-        if i % 6 == 5:
-            image += ":"
-        elif mins > 0:
-            image += "9"
-            mins -= 3
-        else:
-            image += "0"
-    return Image(image)
+class Schedule:
+    def __init__(self, interval=1000):
+        self.interval = interval
+        self.last_update_ticks = utime.ticks_ms()
 
-class Timer():
+    def reset(self):
+        self.last_update_ticks = utime.ticks_ms()
+
+    def tick(self):
+        """Call me in the event loop"""
+        now = utime.ticks_ms()
+        while utime.ticks_diff(now, self.last_update_ticks) >= self.interval:
+            self._fire()
+            self.last_update_ticks += self.interval
+
+    def _fire(self):
+        """Override me"""
+        pass
+
+class Alarm(Schedule):
+    def __init__(self, pattern, repeat=False, interval=100):
+        super().__init__(interval)
+        self.pattern = pattern
+        self.repeat = repeat
+        self.active = None
+
+    def set(self):
+        self.active = self.pattern
+
+    def reset(self):
+        super().reset()
+        self.active = None
+
+    def _fire(self):
+        if self.active is None:
+            return
+        pin0.write_digital(int(self.active[0]))
+        self.active = self.active[1:]
+        if len(self.active) == 0:
+            if self.repeat:
+                self.active = self.pattern
+            else:
+                self.active = None
+
+class Timer(Schedule):
     def __init__(self):
-        self.last_update_ticks = None
+        super().__init__(60000)
         self.mins_left = None
-        self.alarm = False
+        self.half_time_alarm = Alarm("111000111000111000")
+        self.countdown_alarm = Alarm("1010")
+        self.final_alarm = Alarm("111000", True)
+        self.half_time = None
 
     def _draw(self):
-        display.show(get_time_image(self.mins_left))
+        mins = self.mins_left
+        if mins <= 9:
+            display.show(str(mins))
+        else:
+            image = ""
+            for i in range(29):
+                if i % 6 == 5:
+                    image += ":"
+                elif mins > 0:
+                    image += "9"
+                    mins -= 3
+                else:
+                    image += "0"
+            display.show(Image(image))
 
     def start(self, mins):
         self.mins_left = mins
-        self.last_update_ticks = time.ticks_ms()
-        self.alarm = False
+        self.half_time_alarm.reset()
+        self.countdown_alarm.reset()
+        self.final_alarm.reset()
+        if mins >= 20:
+            self.half_time = mins // 2
+        else:
+            self.half_time = None
+        self.reset()
         self._draw()
 
-    def update(self):
-        now = time.ticks_ms()
-        if time.ticks_diff(now, self.last_update_ticks) >= 60000:
-            if self.mins_left > 0:
-                self.mins_left -= 1
-                if self.mins_left == 0:
-                    self.alarm = True
-            self._draw()
-            self.last_update_ticks = now
+    def tick(self):
+        super().tick()
+        self.half_time_alarm.tick()
+        self.countdown_alarm.tick()
+        self.final_alarm.tick()
+
+    def _fire(self):
+        if self.mins_left > 0:
+            self.mins_left -= 1
+            if self.mins_left == 0:
+                self.final_alarm.set()
+            elif self.mins_left == self.half_time:
+                self.half_time_alarm.set()
+            elif self.mins_left <= 5:
+                self.countdown_alarm.set()
+        self._draw()
 
     def incr(self):
         fill_up = 15 - self.mins_left % 15
@@ -57,24 +115,12 @@ class Timer():
             else:
                 self.mins_left -= 15
             self.start(self.mins_left)
-    
-    def check_alarm(self):
-        alarm = self.alarm
-        self.alarm = False
-        return alarm
-
-def beep():
-    for i in range(1, 7):
-        pin0.write_digital(i % 2)
-        time.sleep_ms(500)
 
 timer = Timer()
-timer.start(30)
+timer.start(60)
 while True:
     if button_a.was_pressed():
         timer.incr()
     if button_b.was_pressed():
         timer.decr()
-    timer.update()
-    if timer.check_alarm():
-        beep()
+    timer.tick()
